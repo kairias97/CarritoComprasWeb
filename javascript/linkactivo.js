bff65a2c679f6ab3374c271bb408234d1501f6a6
Kevin Irías <kevin.irias@inaxir.com>
2016-09-11 15:25:53 -0600

    Mejoras en navegabilidad
    Se mejoró la barra de navegación y se agregó navegabilidad hasta los
    detalles de cada producto.

---
+++ javascript/linkactivo.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+        $('#actual').bind('click', function(){
+            return false;
+        })
+});
--- javascript/popup.js
+++ javascript/popup.js
@@ -1,10 +1,11 @@
-$('div.filaItemCarrito img').hover(function (e) {
+$(document).ready(function(){
+    $('div.filaItemCarrito img').hover(function (e) {
 
     var target = '#' + ($(this).attr('data-cuadro-emergente'));
     $(target).show();
 }).mouseout(function(e){
     var target = '#' + ($(this).attr('data-cuadro-emergente'));
 	$(target).hide();
-
-
+});
+    
 });
